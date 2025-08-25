c888e39a57e538754f825ef88851fd871aad49bb
s-bashar <88164841+s-bashar@users.noreply.github.com>
2025-08-24 23:34:43 -0700

    Lesson_1_Part1and2: Rendering Pixels and liens

--- model.cpp
+++ model.cpp
@@ -1,80 +1,24 @@
-#include <sstream>
-#include "model.h"
+#include "tgaimage.h"
 
-Model::Model(const std::string filename) {
-    std::ifstream in;
-    in.open(filename, std::ifstream::in);
-    if (in.fail()) return;
-    std::string line;
-    while (!in.eof()) {
-        std::getline(in, line);
-        std::istringstream iss(line.c_str());
-        char trash;
-        if (!line.compare(0, 2, "v ")) {
-            iss >> trash;
-            vec3 v;
-            for (int i : {0,1,2}) iss >> v[i];
-            verts.push_back(v);
-        } else if (!line.compare(0, 3, "vn ")) {
-            iss >> trash >> trash;
-            vec3 n;
-            for (int i : {0,1,2}) iss >> n[i];
-            norms.push_back(normalized(n));
-        } else if (!line.compare(0, 3, "vt ")) {
-            iss >> trash >> trash;
-            vec2 uv;
-            for (int i : {0,1}) iss >> uv[i];
-            tex.push_back({uv.x, 1-uv.y});
-        }  else if (!line.compare(0, 2, "f ")) {
-            int f,t,n, cnt = 0;
-            iss >> trash;
-            while (iss >> f >> trash >> t >> trash >> n) {
-                facet_vrt.push_back(--f);
-                facet_tex.push_back(--t);
-                facet_nrm.push_back(--n);
-                cnt++;
-            }
-            if (3!=cnt) {
-                std::cerr << "Error: the obj file is supposed to be triangulated" << std::endl;
-                return;
-            }
-        }
-    }
-    std::cerr << "# v# " << nverts() << " f# "  << nfaces() << " vt# " << tex.size() << " vn# " << norms.size() << std::endl;
-    auto load_texture = [&filename](const std::string suffix, TGAImage &img) {
-        size_t dot = filename.find_last_of(".");
-        if (dot==std::string::npos) return;
-        std::string texfile = filename.substr(0,dot) + suffix;
-        std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
-    };
-    load_texture("_diffuse.tga",    diffusemap );
-    load_texture("_nm_tangent.tga", normalmap  );
-    load_texture("_spec.tga",       specularmap);
-}
+constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green   = {  0, 255,   0, 255};
+constexpr TGAColor red     = {  0,   0, 255, 255};
+constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-const TGAImage& Model::diffuse()  const { return diffusemap;  }
-const TGAImage& Model::specular() const { return specularmap; }
-int Model::nverts() const { return verts.size(); }
-int Model::nfaces() const { return facet_vrt.size()/3; }
-
-vec3 Model::vert(const int i) const {
-    return verts[i];
-}
-
-vec3 Model::vert(const int iface, const int nthvert) const {
-    return verts[facet_vrt[iface*3+nthvert]];
-}
-
-vec3 Model::normal(const vec2 &uvf) const {
-    TGAColor c = normalmap.get(uvf[0]*normalmap.width(), uvf[1]*normalmap.height());
-    return vec3{(double)c[2],(double)c[1],(double)c[0]}*2./255. - vec3{1,1,1};
-}
-
-vec2 Model::uv(const int iface, const int nthvert) const {
-    return tex[facet_tex[iface*3+nthvert]];
-}
-
-vec3 Model::normal(const int iface, const int nthvert) const {
-    return norms[facet_nrm[iface*3+nthvert]];
-}
-
+//int main(int argc, char** argv) {
+//    constexpr int width  = 64;
+//    constexpr int height = 64;
+//    TGAImage framebuffer(width, height, TGAImage::RGB);
+//
+//    int ax =  7, ay =  3;
+//    int bx = 12, by = 37;
+//    int cx = 62, cy = 53;
+//
+//    framebuffer.set(ax, ay, white);
+//    framebuffer.set(bx, by, white);
+//    framebuffer.set(cx, cy, white);
+//
+//    framebuffer.write_tga_file("framebuffer.tga");
+//    return 0;
+//}
